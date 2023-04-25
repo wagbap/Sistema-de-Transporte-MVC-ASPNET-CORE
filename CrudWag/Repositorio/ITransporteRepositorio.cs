1c0383f68faba7abc19176be25dd30f96d88e1b8
Wagner Baptista <wagner.h2@hotmail.com>
2023-04-25 23:16:54 +0100

    interface Transport done

---
+++ CrudWag/Repositorio/ITransporteRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using CrudWag.Models;
+
+namespace CrudWag.Repositorio
+{
+    public interface ITransporteRepositorio
+    {
+        List<TransporteModel> ListarTodos();
+
+        TransporteModel Create(TransporteModel transporte);
+        TransporteModel Adicionar(TransporteModel transporte);
+        TransporteModel ListarPorId(int id);
+        TransporteModel Atualizar(TransporteModel transporte);
+        bool Apagar(int id);
+
+       
+
+    }
+}
---
+++ CrudWag/Repositorio/TransporteRepositorio.cs
@@ -0,0 +1,84 @@
+﻿using CrudWag.Data;
+using CrudWag.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace CrudWag.Repositorio
+{
+    public class TransporteRepositorio : ITransporteRepositorio
+    {
+        private readonly BancoDbContext _bancoDbContext;
+        public TransporteRepositorio(BancoDbContext bancoDbContext)
+        {
+            _bancoDbContext = bancoDbContext;
+        }
+
+      
+        public bool Apagar(int id)
+        {
+            var transporte = ListaPorId(id);
+            if (transporte == null) return false;
+
+            if (!string.IsNullOrEmpty(transporte.ImageURL))
+            {
+                ExcluirImagemAntiga(transporte.ImageURL);
+                transporte.ImageURL = transporte.ImageURL;
+            }
+            _bancoDbContext.TbTransporte.Remove(transporte);
+            _bancoDbContext.SaveChanges();
+            return true;
+        }
+
+        public void ExcluirImagemAntiga(string imageUrl)
+        {
+            if (!string.IsNullOrEmpty(imageUrl))
+            {
+                var imagePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot" + imageUrl);
+                if (System.IO.File.Exists(imagePath))
+                {
+                    System.IO.File.Delete(imagePath);
+                }
+            }
+        }
+
+
+        
+        public List<TransporteModel> BuscarTodos()
+        {
+            return _bancoDbContext.TbTransporte.ToList();
+        }
+
+
+        public TransporteModel Create(TransporteModel transporte)
+        {
+            _bancoDbContext.TbTransporte.Add(transporte);
+            _bancoDbContext.SaveChanges();
+            return transporte;
+        }
+
+        public TransporteModel ListaPorId(int id)
+        {
+            return _bancoDbContext.TbTransporte.FirstOrDefault(x => x.Id == id);
+        }
+
+        public TransporteModel Atualizar(TransporteModel transporte)
+        {
+            TransporteModel transporteDB = ListaPorId(transporte.Id);
+
+            if (transporteDB == null) throw new Exception("nao encontrado");
+
+            transporteDB.ImageURL = transporte.ImageURL;
+            transporteDB.Modelo = transporte.Modelo;
+            transporteDB.Ano = transporte.Ano;
+            transporteDB.CapacidadePassageiro = transporte.CapacidadePassageiro;
+            transporteDB.TaxaAluguer = transporte.TaxaAluguer;
+            transporteDB.IsAvailable = transporte.IsAvailable;
+            transporteDB.TransporteTipo = transporte.TransporteTipo;
+
+
+            _bancoDbContext.TbTransporte.Update(transporteDB);
+            _bancoDbContext.SaveChanges();
+            return transporteDB;
+        }
+     
+    }
+}
